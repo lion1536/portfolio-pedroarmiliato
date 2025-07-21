2d3349ebe43e7aea60875a12787d07e1d2be71bf
Pedro <pedroarmiliato2006@gmail.com>
2025-07-21 16:34:47 -0300

    Consertar o cors

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const apiRoutes = require("./routes/api");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-app.use(bodyParser.json());
-app.use("/api", apiRoutes);
 
 app.use(
   cors({
@@ -22,6 +18,8 @@ app.use(
 );
 
 app.use(express.json());
+
+app.use("/api", apiRoutes);
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando na porta http://0.0.0.0:${PORT}`);
