49d6c204c9aa4cbe777415aecce0b8fbc702bbf2
PeterBytesTB <pedropowerbyte@gmail.com>
2025-07-21 21:27:27 -0300

    server log

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,33 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const apiRoutes = require("./routes/api");
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const apiRoutes = require('./routes/api')
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const PORT = process.env.PORT || 8080;
+const app = express()
+const PORT = process.env.PORT || 8080
+
+// Log para conferir se as variáveis de ambiente estão carregando
+console.log('EMAIL_USER:', process.env.EMAIL_USER || 'Não definido')
+console.log(
+  'EMAIL_PASSWORD:',
+  process.env.EMAIL_PASSWORD ? '****************' : 'Não definido',
+)
 
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
+    origin: '*',
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type'],
     credentials: false,
-  })
-);
+  }),
+)
 
-app.use(express.json());
+app.use(express.json())
 
-app.use("/api", apiRoutes);
+app.use('/api', apiRoutes)
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor rodando na porta http://0.0.0.0:${PORT}`);
-});
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor rodando na porta http://0.0.0.0:${PORT}`)
+})
